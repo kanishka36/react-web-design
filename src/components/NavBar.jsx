d688ee0840f3f8d5813b68fadf6781a7da6e7f08
kanishka <kanishkaj36@gmail.com>
2023-04-30 22:35:41 +0530

    I have update navbar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import { links } from '../data';
 import Logo from '../Images/logo2.png';
-import {GoThreeBars} from 'react-icons/go';
 import './navBar.css'
 
 function NavBar() {
+
+    const [isMenuShowing, setIsMenuShowing] = useState(false);
+  
+
   return (
     <nav>
-        <div className="container nav__container">
+        <div className={`container nav__container ${isMenuShowing ? 'nav__border':''}`}>
             <div className="nav__container-wrapper">
-                <div className="nav__icon">
-                    
+                <div className="nav__icon" onClick={()=> setIsMenuShowing(!isMenuShowing)}>
+                    <div className="icon__lines">
+                        <div className={`line-1 ${isMenuShowing ? 'is-line-1-rotate':''}`}></div>
+                        <div className={`line-2 ${isMenuShowing ? 'is-line-show':''}`}></div>
+                        <div className={`line-3 ${isMenuShowing ? 'is-line-show':''}`}></div>
+                        <div className={`line-4 ${isMenuShowing ? 'is-line-4-rotate':''}`}></div>
+                    </div>
+                </div>
+                <div className="logo">
+                    <Link to="/">
+                        <img src={Logo} alt="Nav Logo" />
+                    </Link>
                 </div>
             </div>
-            <Link to="/" className='logo'>
-                <img src={Logo} alt="Nav Logo" />
-            </Link>
-            <ul className="nav__links">
-                {
-                    links.map(({name,path},index)=>{
-                        return(
-                            <li>
-                                <NavLink to={path} key={index}>{name}</NavLink>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-
-            
         </div>
+        <div className={`nav__menu ${isMenuShowing ? '':'menu__show'}`}>
+            <div className="nav__links">
+                <div className="menu__wrapper">
+                    <ul className='nav__links'>
+                        {
+                            links.map(({name, path},index)=> {
+                                return (
+                                    <li key={index}>
+                                        <NavLink to={path}>{name}</NavLink>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+        
     </nav>
   )
 }
