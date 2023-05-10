9223042ea34038b057c995f1b7a882b8b7c76e3b
kanishka <kanishkaj36@gmail.com>
2023-05-10 13:45:44 +0530

    I updated header

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header'
+import './home.css'
 
 function Home() {
   return (
     <>
       <Header/>
+      
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Video from '../videos/header-video.mp4'
+import Video from '../videos/blue-sky.mp4'
 import './header.css'
 
 function Header() {
   return (
-    <header className='container'>
-        <div className="header-container">
-            <div className="header-left">
-                    <h2>WE REWRITE<br/> THE RULES OF DIGITAL<br/> TRANSFORMATION</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nesciunt inventore atque natus, impedit aliquam est optio quasi.</p>
-                    <button className='header-button'>
-                    <Link to="/about" className='btn lg'>READ MORE</Link> 
-                    </button> 
-      
-            </div>
-            <div className="header-right">
-                <div className="header-video">
-                    <video autoPlay loop>
-                        <source src={Video} type='video/mp4'/>
-                    </video>
-                </div>
-            </div>
-        </div>
-            
-    </header>
+    <>
+      <header>
+          <video autoPlay loop>
+              <source src={Video} type='video/mp4'/>
+          </video>
+          <div className="text__container">
+              <h1>CUSTOM SOFTWARE <br/>& WEB DEVELOPMENT <br/>TO FUEL YOUR GROWTH</h1>
+              <Link to='/about'>Read More</Link>
+          </div>
+      </header>
+    </>
+
   )
 }
 
