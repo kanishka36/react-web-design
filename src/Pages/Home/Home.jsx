695b3fe3cdcf09b6f6a488749b9801a25d346bee
kanishka <kanishkaj36@gmail.com>
2023-05-16 13:08:50 +0530

    update sections

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import Header from '../../components/Header'
-import SECTION_B from './SECTION_B'
-import SECTION_A from './SECTION_A'
-import SECTION_C from './SECTION_C'
+import SECTION_A from '../Home/SECTION_A'
+import SECTION_B from '../Home/SECTION_B'
+import SECTION_C from '../Home/SECTION_C'
 import Footer from '../../components/Footer'
 import './home.css'
 
 
 function Home() {
   return (
-    <>
+    <div className='home__container'>
       <Header/>
       <SECTION_A/>
       <SECTION_B/>
       <SECTION_C/>
       <Footer/>
-    </>
+    </div>
   )
 }
 
--- src/Pages/Home/SECTION_C.jsx
+++ src/Pages/Home/SECTION_C.jsx
@@ -3,17 +3,17 @@ import image from '../../Images/shaped.png'
 
 function SECTION_C() {
   return (
-    <section className='section-c'>
-      <div className="discription__container">
+    <section className='section'>
+      <div className="elementary__container">
+        <img src={image} alt="shaped" />
+        <div className="elementary-header">
           <h1>BE WORTH THEIR TIME</h1>
           <h2>THEIR TIME WILL BE WORTH YOUR MONEY</h2>
-          <div className="discription">
-            <p>We believe building meaningful relationships with the audience is just as important as building revenue. At Centrick, we rely on data to create content that is rooted in sentiment. We put search at the heart of everything we create. This helps us design solutions that are always Google first or Amazon friendly. Yes! We built Centrick on the crossroads of data, technology and creativity. Here, Art, Copy and Code mingle together with mutual respect to create one thing, and one thing only, Magic.</p>
-          </div>
+          <p>We believe building meaningful relationships with the audience is just as important as building revenue. At Centrick, we rely on data to create content that is rooted in sentiment. We put search at the heart of everything we create. This helps us design solutions that are always Google first or Amazon friendly. Yes! We built Centrick on the crossroads of data, technology and creativity. Here, Art, Copy and Code mingle together with mutual respect to create one thing, and one thing only, Magic.</p>
         </div>
-        <img src={image} alt="shape-img" />
+      </div>
     </section>
   )
 }
 
-export default SECTION_C
+export default SECTION_C;
